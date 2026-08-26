2f7cde0a7d8c82fe05a6f8c83ec0bba4d3749c86
Jingyan Dong <jdong@f0r3s17.codam.nl>
2026-08-26 17:03:33 +0200

    add a struct in config. start working on processing config file

--- include/Config.hpp
+++ include/Config.hpp
@@ -1,8 +1,21 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+#include <string>
+
+struct ServerConfig
+{
+	std::string	host = "";
+	int			port = 0;
+	std::string	serverName = "localhost";
+	//std::string	root = "./www";
+	//std::string	index = "index.html";
+};
+
+
 class Config
 {
 };
+
 
 #endif
