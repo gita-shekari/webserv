8fa9e4bfa9b2b8d8c9067919334f7c55c23923cb
Hollie Gu <gu.manhan@outlook.com>
2026-08-27 13:45:58 +0200

    http_parser

--- RequestParser.hpp
+++ RequestParser.hpp
@@ -13,7 +13,21 @@ enum	ParseSection
 {
 	REQUEST_LINE,
 	HEADERS,
-	BODY
+	BODY,
+	DONE
+};
+
+enum	BodyType
+{
+	NO_BODY,
+	CHUNKED_BODY,
+	CONTENT_LENGTH_BODY
+};
+
+enum	ChunkState
+{
+	CHUNK_SIZE,
+	CHUNK_DATA
 };
 
 class RequestParser
@@ -21,12 +35,34 @@ class RequestParser
 	public:
 		RequestParser();
 		ParseStatus	parse(const std::string& buffer, Request& req);
+		size_t	getConsumedBytes() const;
+		void	reset();
+
+		//debug
+		void	printAttributes() const;
 
 	private:
 		ParseSection	_section;
 		size_t			_cursor;
 		size_t			_contentLength;
+		BodyType		_body;
+		ChunkState		_chunkState;
+		size_t			_chunkSize;
 
 		ParseStatus	parseRequestLine(const std::string& buffer, Request& req);
+		ParseStatus	parseHeaders(const std::string& buffer, Request& req);
+		ParseStatus	parseBody(const std::string& buffer, Request& req);
 
-}
+		ParseStatus	judgeBody(const Request& req);
+
+		ParseStatus	parseContentLengthBody(const std::string& buffer, Request& req);
+		ParseStatus	parseChunkedBody(const std::string& buffer, Request& req);
+
+		//parse size
+		bool	parseDecSize(const std::string& str, size_t& size);
+		bool	parseHexSize(const std::string& str, size_t& size);
+	
+		//utils
+		std::string	toLower(const std::string& str);
+		std::string	trim(const std::string& str);
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,53 @@
-#include "webserv.hpp"
+#include "RequestParser.hpp"
+#include "Request.hpp"
 
-int	main()
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+std::string	statusToString(ParseStatus status)
 {
-	webserv	server;
+	if (status == INCOMPLETE)
+		return "Incomplete";
+	else if (status == COMPLETE)
+		return "Complete";
+	else if (status == ERROR)
+		return "Error";
+	else
+		return "Unknown";
+}
 
-	if (!server.init())
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+		std::cerr << "Error" << std::endl;
+
+	std::ifstream file(argv[1]);
+	if (!file)
 		return 1;
-	server.run();
+	
+	std::stringstream	ss;
+	ss << file.rdbuf();
+
+	std::string buffer = ss.str();
+
+	Request	req;
+	RequestParser	parser;
+
+	ParseStatus status = parser.parse(buffer, req);
+	std::cout << "------------------\nParsing Status: " << statusToString(status) << std::endl;
+	parser.printAttributes();
+	req.printRequest();
+	// req.clear();
+
+	// code for partial_chunk test
+	buffer += "ld\r\n0\r\n\r\n";
+	status = parser.parse(buffer, req);
+	std::cout << "------------------\nParsing Status: " << statusToString(status) << std::endl;
+	parser.printAttributes();
+	req.printRequest();
+
 	return 0;
 }
