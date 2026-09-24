db742f00a549db4066ed92084ced017073f49c29
Gita Shekari <gshekari@f0r1s14.codam.nl>
2026-09-24 13:42:54 +0200

    update RsponseBuilder.hpp

--- include/ResponseBuilder.hpp
+++ include/ResponseBuilder.hpp
@@ -1,25 +1,3 @@
-// #pragma once
-
-// # include "Http.hpp"
-// # include <string>
-// # include <iostream>
-// # include <map>
-// # include <vector>
-// # include <algorithm>
-// # include "Config.hpp"
-
-// class ResponseBuilder
-// {
-// 	public:
-// 		Response buildResponse(const Request& request, const ServerConfig& serverConfig);
-// 		Response buildGetResponse(const Request& request, const ServerConfig& serverConfig);
-// 		Response buildErrorResponse(int statusCode, const ServerConfig& serverConfig);
-// 		const LocationConfig* findLocation(const std::string& path, const ServerConfig& serverConfig);
-// 		std::string serialize(const Response& response);
-// 	private:
-// 		bool getSource(const std::string& path, std::string& content);
-// };
-
 #pragma once
 
 # include "Http.hpp"
